348a7a7780d983efd756614728b56d5968dc633e
PythonicRinaT <rinatseferov18@gmail.com>
2026-03-25 14:55:15 +0400

    Create Hello world.py
    Hello world 1.1v

---
+++ Hello world.py
@@ -0,0 +1,8 @@
+import random
+print("Hello world 1.1v")
+a=random.randint(1,20)
+b=random.randint(1,20)
+cavab=a+b
+düzgün=input(f"how much is {a}+{b}?  ")
+if düzgün==str(cavab):        print("true")
+else:          print("wrong")
