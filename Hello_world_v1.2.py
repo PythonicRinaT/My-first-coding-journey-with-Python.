c49a3b419a866c5418447fdecb47277215c26fc8
PythonicRinaT <rinatseferov18@gmail.com>
2026-03-27 21:13:49 +0400

    Create Hello_world_v1.2.py

---
+++ Hello_world_v1.2.py
@@ -0,0 +1,9 @@
+print("hello worldv1.2")
+rinat = ["blue", "green", "yellow"]
+rinat.insert(1, "orange")
+rinat.remove("blue")
+rinat2 = ["pink", "purple"]
+rinat.extend(rinat2)
+rinat.reverse()
+rinat.pop()
+print(rinat)
